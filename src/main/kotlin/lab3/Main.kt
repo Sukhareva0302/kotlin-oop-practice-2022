b3c3897c91a814849d96001a12d1c7bcaca87d1e
Сухарева0302 <vl_sukhareva@mail.ru>
2022-04-22 20:36:08 +0300

    added completed task for lab3

--- src/main/kotlin/lab3/Main.kt
+++ src/main/kotlin/lab3/Main.kt
@@ -6,6 +6,7 @@ fun main() {
     service.addContact(Person("Abc", "Def"), phone)
     service.addPhone(Person("Aaadds", "GdJkf"), "4782637", PhoneType.Home)
     service.addEmail(Person("Abc", "Def"), "abc@der.ee")
+    service.addPhone(Person("Abc", "Def"), "4278904", PhoneType.Work)
     service.addAddress(Person("Aaadds", "GdJkf"), "Aaa", "Bbbbb", 4, 2)
     service.addLink(Person("Ddd", "Ff"), "PeopleEe", "ppl.com/123p")
     service.addContact(Person("Gdd", "Ff"), Contact.Link("PeopleEe", "ppl.com/123b"))
